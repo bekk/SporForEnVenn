f5442e99e80bb7d34e1a9647680bafcaf92707c9
Steffen <Steffen.Sande@bekk.no>
2021-04-07 10:14:03 +0200

    La til espen til botten

--- src/main/kotlin/Bekk/no/util.kt
+++ src/main/kotlin/Bekk/no/util.kt
@@ -28,8 +28,9 @@ const val jorundAmsenID = "U6HG03FSB"
 const val steffenID = "ULX53HFMF"
 const val steffenTestID = "U01EEHXK0GY"
 const val andersID = "UB91B9UJY"
+const val espenID = "U02HEARQ0"
 
-val authorizedUsers = listOf(juneID, jorundAmsenID, steffenID, andersID, steffenTestID)
+val authorizedUsers = listOf(juneID, jorundAmsenID, steffenID, andersID, steffenTestID, espenID)
 
 
 val AIRTABLE_API_KEY: String = "Bearer ${System.getenv("AIR_TABLE")}"
